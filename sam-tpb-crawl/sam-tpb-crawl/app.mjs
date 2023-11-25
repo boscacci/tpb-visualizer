c5c8b3bdc1e63482f01c8412a38bc758fea9ffd7
Robert Boscacci <cinemarob1@gmail.com>
2023-11-25 13:23:57 -0500

    Read page title with playwright

--- sam-tpb-crawl/sam-tpb-crawl/app.mjs
+++ sam-tpb-crawl/sam-tpb-crawl/app.mjs
@@ -1,18 +1,30 @@
+import playwright from 'playwright-aws-lambda';
 import AWS from 'aws-sdk';
 const ssm = new AWS.SSM({ region: 'us-east-2' });
+
 let SCRAPER_API_KEY;
 
-const assert = require('node:assert');
-const { chromium, devices } = require('playwright');
-
 export const lambdaHandler = async (event, context) => {
+    let browser = null;
     try {
         if (!SCRAPER_API_KEY) {
-            // console.log("Fetching SCRAPER_API_KEY from SSM...");
             const response = await ssm.getParameter({ Name: 'SCRAPER_API_KEY', WithDecryption: false }).promise();
             SCRAPER_API_KEY = response.Parameter.Value;
-            // console.log("Fetched SCRAPER_API_KEY: ", !!SCRAPER_API_KEY);
         }
+        try {
+            browser = await playwright.launchChromium();
+            const context = await browser.newContext();
+            const page = await context.newPage();
+            await page.goto(event.url || `https://api.scraperapi.com/?api_key=${SCRAPER_API_KEY}&url=https://thepiratebay.org/search.php?q=top100:all`);
+            console.log('Page title: ', await page.title());
+        } catch (error) {
+            throw error;
+        } finally {
+            if (browser) {
+                await browser.close();
+            }
+        }
+
         const response = {
             'statusCode': 200,
             'body': JSON.stringify({
