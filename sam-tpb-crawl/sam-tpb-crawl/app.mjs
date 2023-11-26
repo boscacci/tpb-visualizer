3e007bf0382f26d967aeecdc9fe1212e9d765fde
Robert Boscacci <cinemarob1@gmail.com>
2023-11-26 09:34:20 -0500

    Set up lambdas to start parsing

--- sam-tpb-crawl/sam-tpb-crawl/app.mjs
+++ sam-tpb-crawl/sam-tpb-crawl/app.mjs
@@ -16,7 +16,23 @@ export const lambdaHandler = async (event, context) => {
             const context = await browser.newContext();
             const page = await context.newPage();
             await page.goto(event.url || `https://api.scraperapi.com/?api_key=${SCRAPER_API_KEY}&url=https://thepiratebay.org/search.php?q=top100:all`);
+
             console.log('Page title: ', await page.title());
+
+            const dataTable = await page.locator('#torrents').innerHTML();
+            if (dataTable.length < 500) {
+                throw new Error('Data table has less than 500 characters');
+            }
+            const s3 = new AWS.S3({ region: 'us-east-2' });
+            const date = new Date();
+            const formattedDate = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}_${date.getHours()}-${date.getMinutes()}`;
+            const params = {
+                Bucket: 'tpb-snapshots-html',
+                Key: `tbp_top100_${formattedDate}.html`,
+                Body: dataTable
+            };
+            await s3.putObject(params).promise();
+
         } catch (error) {
             throw error;
         } finally {
@@ -28,7 +44,7 @@ export const lambdaHandler = async (event, context) => {
         const response = {
             'statusCode': 200,
             'body': JSON.stringify({
-                message: "Good job",
+                message: "HTML written to S3",
             })
         };
         return response;
